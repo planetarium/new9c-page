adbf7cab92fd363e4076d41bb2acedb549b6de04
ulismoon (hyeon) <hyeon@planetariumhq.com>
2025-02-06 00:40:05 +0900

    Add initialize

--- telegram.js
+++ telegram.js
@@ -5,44 +5,63 @@ Telegram.WebApp.ready();
 
 // Get User Data
 const initData = Telegram.WebApp.initDataUnsafe;
-const userData = initData.user;
+let userData = initData.user;
 
-async function sendUserDataToServer(unityInstance) {
+const checkTelegramData = () => {
     if (!userData) {
         console.log("User data not available.");
-        document.getElementById("login-button").style = "display:block";
-    } else {
-        const response = await fetch(`${host}/api/auth/login/telegram`, {
-            method: 'POST',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
-                id: userData.id,
-                username: userData.username ?? userData.first_name,
-                is_bot: userData.is_bot ?? false,
-                is_premium: userData.is_premium ?? false,
-                allows_write_to_pm: userData.allows_write_to_pm,
-                photo_url: userData.photo_url ?? "",
-            }),
-        });
+        document.getElementById("modal").style.visibility = "visible";
+        return false;
+    }
+    return true;
+};
 
-        const result = await response.json();
-        console.log("=======");
-        console.log(result);
-        console.log("=======");
-        if (response.success) {
-            console.log("Connected successfully!");
-        } else {
-            console.log("Connection failed: " + result.message);
+const onTelegramLogin = async (user) => {
+    const resp = await fetch(`${host}/api/auth/validate-telegram`,
+        {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(user),
         }
-        token = result.token;
-        unityInstance.SendMessage("APIClient", "SetToken", "Bearer " + result.token);
-        unityInstance.SendMessage("APIClient", "SetUri", host);
+        );
+    if (resp.status === 200 && (await resp.json()).valid === true) {
+        user.is_bot = false;
+        user.is_premium = false;
+        user.allows_write_to_pm =false;
+        userData = user;
+        document.getElementById("modal").style.visibility = "hidden";
+        initUnity();
     }
 }
 
-const telegramLogin = async() => {
-    alert("login")
-};
+async function telegramLogin(unityInstance) {
+    const response = await fetch(`${host}/api/auth/login/telegram`, {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+            id: userData.id,
+            username: userData.username ?? userData.first_name,
+            is_bot: userData.is_bot ?? false,
+            is_premium: userData.is_premium ?? false,
+            allows_write_to_pm: userData.allows_write_to_pm,
+            photo_url: userData.photo_url ?? "",
+        }),
+    });
 
-window.sendUserDataToServer = sendUserDataToServer;
+    const result = await response.json();
+    console.log("=======");
+    console.log(result);
+    console.log("=======");
+    if (response.status === 200 && result.token) {
+        console.log("Connected successfully!");
+    } else {
+        console.log("Connection failed: " + result.message);
+    }
+    token = result.token;
+    unityInstance.SendMessage("APIClient", "SetToken", "Bearer " + result.token);
+    unityInstance.SendMessage("APIClient", "SetUri", host);
+    unityInstance.SendMessage("APIClient", "Initialize");
+}
+
+window.onTelegramLogin = onTelegramLogin;
 window.telegramLogin = telegramLogin;
